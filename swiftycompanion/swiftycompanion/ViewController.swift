5874caac84feedb3b3a79182e9dd7d8f1b05ce6a
Antoine JOUANNAIS <ajouanna@student.42.fr>
2017-04-18 16:04:34 +0200

    premier commit

---
+++ swiftycompanion/swiftycompanion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftycompanion
+//
+//  Created by Antoine JOUANNAIS on 4/18/17.
+//  Copyright © 2017 Antoine JOUANNAIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
