8fb7acfa933fcffea8e61b2239e89e3397445e9a
Antoine JOUANNAIS <ajouanna@student.42.fr>
2017-04-20 19:05:46 +0200

    exxtraction des 1eres deonnes

--- swiftycompanion/swiftycompanion/DetailsViewController.swift
+++ swiftycompanion/swiftycompanion/DetailsViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var imageField: UIImageView!
     @IBOutlet weak var loginField: UILabel!
+    
+    @IBOutlet weak var emailField: UILabel!
+    @IBOutlet weak var firstNameField: UILabel!
+    @IBOutlet weak var lastNameField: UILabel!
+    @IBOutlet weak var phoneField: UILabel!
+    
     var login = ""
+    var loginVC : ViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         loginField.text = login
+        getFrom42(login)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,5 +44,77 @@ class DetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func getFrom42(_ str : String) {
+        print("getFrom42(\(str))")
+        
+        if let token = loginVC?.token {
+            
+            let q = str.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+            let url = "https://api.intra.42.fr/v2/users/\(q)"
+            print("url : \(url)")
+            let my_mutableURLRequest = NSMutableURLRequest(url: URL(string : url)!)
+            my_mutableURLRequest.httpMethod = "GET"
+            my_mutableURLRequest.setValue("Bearer " +  token, forHTTPHeaderField: "Authorization")
+            
+            let task = URLSession.shared.dataTask(with: my_mutableURLRequest as URLRequest, completionHandler: {
+                (data, response, error) in
+                if let err = error {
+                    print("Erreur sur requete web : \(err)")
+                } else if let d = data {
+                    DispatchQueue.main.async {
+                    do {
+//                        if let responseObject = try JSONSerialization.jsonObject(with: d, options: []) as? [String:AnyObject],
+//                            let arrayStatuses = responseObject["statuses"] as? [[String:AnyObject]] {
+                            if let responseObject = try JSONSerialization.jsonObject(with: d, options: []) as? [String:AnyObject] {
+                                print("responseObject: \(responseObject)")
+                                if let email = responseObject["email"] as! String? {
+                                    self.emailField.text = email
+                                }
+                                if let first_name = responseObject["first_name"] as! String? {
+                                    self.firstNameField.text = first_name
+                                }
+                                if let last_name = responseObject["last_name"] as! String? {
+                                    self.lastNameField.text = last_name
+                                }
+                                if let phone = responseObject["phone"] as! String? {
+                                    self.phoneField.text = phone
+                                }
+                                if let image_url = responseObject["image_url"] as! String? {
+                                    let url = URL(string: image_url)
+                                    let data = try Data(contentsOf: url!)
+                                    self.imageField.image = UIImage(data: data)
+                                }
+                                
+                                
+                                /*
+                                for status in arrayStatuses {
+                                let text = status["text"] as! String
+                                let user = status["user"]?["name"]  as! String
+                                if let date = status["created_at"] as? String {
+                                    let dateFormatter = DateFormatter()
+                                    dateFormatter.dateFormat = "E MMM dd HH:mm:ss Z yyyy"
+                                    if let date = dateFormatter.date(from: date) {
+                                        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+                                        let newDate = dateFormatter.string(from: date)
+                                    }
+                                }
+ 
+                                print(status)
+                            }
+ */
+                        }
+                        print("Donnees recues de l'API")
+                        // A FINIR
+                        
+                    } catch _{
+                        print("Connexion lost")
+                    }
+                    }
+                }
+            })
+            task.resume()
+        }
+    }
 
 }
--- swiftycompanion/swiftycompanion/ViewController.swift
+++ swiftycompanion/swiftycompanion/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var errorLabel: UILabel!
     
+    @IBOutlet weak var searchButton: UIBarButtonItem!
     @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var activityController: UIActivityIndicatorView!
     
     let consumerKey : String = "f833c3cb76771590d718ea287b618ad9557cf351761e6682ac8d3641a6553a3c"
     let consumerSecret : String = "618e7c8009abfe61ed61ca8b7856851c4f5e3a2c5aa3bdf71365adcbd68b78db"
@@ -23,7 +25,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         initToken()
-
+        loginField.isHidden = true
+        searchButton.isEnabled = false
+        errorLabel.text = "Waiting for token from server..."
+        activityController.hidesWhenStopped = true
+        activityController.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,22 +46,27 @@ class ViewController: UIViewController {
         request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
         request.httpBody = "grant_type=client_credentials".data(using: String.Encoding.utf8)
         let task = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
-            guard let data = data, error == nil else {
-                print(error!)
-                self.errorLabel.text = "Fatal error : can't connect to API server"
-                return
-            }
-            do {
-                print(response!)
-                if let dic: NSDictionary = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-                    print(dic)
-                    self.token = (dic["access_token"] as? String)!
-                    print("token recupéré de 42 : \(String(describing: self.token))")
-
+            DispatchQueue.main.async {
+                self.activityController.stopAnimating()
+                guard let data = data, error == nil else {
+                    print(error!)
+                    self.errorLabel.text = "Fatal error : can't connect to API server"
+                    return
                 }
-            }
-            catch (let err) {
-                print(err)
+                do {
+                    print(response!)
+                    if let dic: NSDictionary = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                        print(dic)
+                        self.token = (dic["access_token"] as? String)!
+                        print("token recupéré de 42 : \(String(describing: self.token))")
+                        self.errorLabel.text = "Ready for input"
+                        self.loginField.isHidden = false
+                        self.searchButton.isEnabled = true
+                    }
+                }
+                catch (let err) {
+                    print(err)
+                }
             }
         }
         task.resume()
@@ -64,7 +75,8 @@ class ViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "detailsSegue" {
             if let vc = segue.destination as? DetailsViewController {
-                vc.login = loginField.text!
+                vc.login = loginField.text!.lowercased()
+                vc.loginVC = self
             }
         }
     }
