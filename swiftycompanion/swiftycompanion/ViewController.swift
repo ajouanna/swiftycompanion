6ad0cb2a804dfbb7e5727c35dc369dee5a2b5e29
Antoine Jouannais <antoine.jouannais@gmail.com>
2017-04-20 10:07:20 +0200

    gestion erreur

--- swiftycompanion/swiftycompanion/ViewController.swift
+++ swiftycompanion/swiftycompanion/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     let consumerKey : String = "f833c3cb76771590d718ea287b618ad9557cf351761e6682ac8d3641a6553a3c"
     let consumerSecret : String = "618e7c8009abfe61ed61ca8b7856851c4f5e3a2c5aa3bdf71365adcbd68b78db"
     var token : String?
@@ -35,12 +38,13 @@ class ViewController: UIViewController {
         request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
         request.httpBody = "grant_type=client_credentials".data(using: String.Encoding.utf8)
         let task = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
-            print(response!)
             guard let data = data, error == nil else {
                 print(error!)
+                self.errorLabel.text = "Fatal error : can't connect to API server"
                 return
             }
             do {
+                print(response!)
                 if let dic: NSDictionary = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
                     print(dic)
                     self.token = (dic["access_token"] as? String)!
